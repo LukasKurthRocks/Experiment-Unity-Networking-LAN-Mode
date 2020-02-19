5ebd3534ac7d0cb98e384b26c022897607cf7e5b
Lukas Kurth <github@lukas.kurth.rocks>
2020-02-19 22:29:24 +0100

    Testing
    Nichts solides bis jetzt ...

--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -1,17 +1,26 @@
-﻿using UnityEngine;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
+using UnityEngine;
 
 public class NetworkManager : Singleton<NetworkManager> {
+    public delegate void PacketHandler(int _fromClient, Packet _packet);
+    public static Dictionary<int, PacketHandler> packetHandlers;
+
     private Socket _socketServer;
     private EndPoint _remoteEndPoint;
+    private Packet _receivedData; // Handling data
+    private byte[] _receiveBuffer;
 
-    void StartServer(int port) {
+    private void Start() {
+        InitializeServerData();
+    }
+
+    public void StartServer(int port) {
         if (_socketServer == null) {
             try {
                 _socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
@@ -35,7 +44,63 @@ public class NetworkManager : Singleton<NetworkManager> {
         }
     }
 
-    void OnReceive(IAsyncResult _asyncResult) {
+    public void CloseServer() {
+        if (_socketServer != null) {
+            _socketServer.Close();
+            _socketServer = null;
 
+            Debug.Log("Closed socket on server.");
+        }
+    }
+
+    // NOT WORKING. COMPARE OTHER SOLUTION
+    void OnReceive(IAsyncResult _asyncResult) {
+        if (_socketServer != null) {
+            try {
+                int size = _socketServer.EndReceiveFrom(_asyncResult, ref _remoteEndPoint);
+                _socketServer.BeginReceiveFrom(new byte[1024], 0, 1024, SocketFlags.None, ref _remoteEndPoint, new AsyncCallback(OnReceive), null);
+
+                if (size <= 0) {
+                    // Disconnecting client/player
+                    return;
+                }
+
+                // Having data, copying bytes into array
+                byte[] _data = new byte[size];
+                Array.Copy(_receiveBuffer, _data, size);
+
+                
+                // Handle data
+                _receivedData.Reset(HandleData(_data));
+
+                /*
+                int _packetId = _packet.ReadInt();
+                _packetHandlers[_packetId](_packet);
+                */
+
+                byte[] str = Encoding.ASCII.GetBytes("pong");
+
+                // Send a pong to the remote (client)
+                _socketServer.SendTo(str, _remoteEndPoint);
+
+            } catch (Exception ex) {
+                Debug.Log(ex.ToString());
+            }
+        }
+    }
+
+    private bool HandleData(byte[] _data) {
+
+
+        return false;
+    }
+
+    private static void InitializeServerData() {
+        // TODO: Initialize Client Array
+
+        packetHandlers = new Dictionary<int, PacketHandler>() {
+            { (int)ClientPackets.ping, ServerReceive.Ping }
+        };
+        Debug.Log("Server::InitializeServerData(): Initialized packets.");
     }
 }
